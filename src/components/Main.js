e8ad98d6d55ce03279fc78175e845003fccfe3b0
Frankie Rodriguez <rfrankie04@gmail.com>
2019-11-30 16:02:43 -0500

    dev branch creation

--- src/components/Main.js
+++ src/components/Main.js
@@ -91,7 +91,30 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">About bloop</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
+            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
+            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
+            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
+            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
+            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
+            amet.
+          </p>
+          {close}
+        </article>
+
+        <article
+            id="about"
+            className={`${this.props.article === 'about' ? 'active' : ''} ${
+                this.props.articleTimeout ? 'timeout' : ''
+            }`}
+            style={{ display: 'none' }}
+        >
+          <h2 className="major">About bloop</h2>
           <span className="image main">
             <img src={pic03} alt="" />
           </span>
